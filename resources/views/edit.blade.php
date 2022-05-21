7708f53c5f54b6b1e9f569072b3ee4e266ddc214
Rwhytm <rajuwahyudi842@gmail.com>
2022-05-21 12:47:33 +0700

    update and create

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body>
+    <form action="{{ route('biodata.ganti', $data->id) }}" method="POST">
+        @method('PUT')
+        @csrf
+
+        <div class="mb-3">
+            <label for="nama" class="form-label">nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" aria-describedby="emailHelp" value="{{ $data->nama }}">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">NPM</label>
+            <input type="text" class="form-control" name="npm" id="exampleInputPassword1" value="{{ $data->npm }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+
+</body>
+</html>
